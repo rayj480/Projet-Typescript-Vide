0c06e8a7adb37cc6f33ffb1809d62de8d72e0c4a
Jonathan CARI <Jonathan CARI>
2017-05-03 12:07:43 +0200

    suppression des éléments inutile

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
-import {ShowTasks} from '../containers'
 
-export interface AppProps { compiler: string; framework: string; }
+export interface AppProps { qui: string; quoi: string; }
 
 // 'AppProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
 export class App extends React.Component<AppProps, undefined> {
     render() {
         return (<div>
-            <h1>App from {this.props.compiler} and {this.props.framework}!</h1>
-            <ShowTasks />
+            <h1>Salut {this.props.qui}, on va faire du {this.props.quoi}!</h1>
         </div>);
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-const createLogger = require('redux-logger').createLogger;
-import thunkMiddleware from 'redux-thunk';
-const Provider = require('react-redux').Provider;
-import {createStore, applyMiddleware} from 'redux';
-import {tachesReducer} from './reducers';
 
 import { App } from "./components/App";
 import "./css/style.scss";
@@ -16,25 +11,9 @@ declare var window: {
     new(): Window;
 }
 
-const enhancer:any = window['devToolsExtension'] ? window['devToolsExtension']()(createStore) : createStore;
-    // const store = enhancer(settingsReducer, initialState);
 
-
-function configureStore(){
-    let loggerMiddleware = createLogger();
-    let middlewares = [thunkMiddleware,loggerMiddleware];
-    let store = createStore(tachesReducer, applyMiddleware(...middlewares));
-    // store.dispatch(fetchUserCourant());
-    // store.dispatch(fecthHistorique());
-
-    return store;
-}
-
-const store = configureStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App compiler="TypeScript" framework="Angular" />
-    </Provider>,
+    <App qui="Matthieu" quoi="REACT" />,
     document.getElementById("example")
 );
