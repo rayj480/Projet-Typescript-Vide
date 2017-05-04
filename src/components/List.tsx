9af42ec4911dc9edd10718654caafc4d329d0c78
Jonathan CARI <Jonathan CARI>
2017-05-04 14:46:47 +0200

    correction des erreurs

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,6 +5,7 @@ interface ListState { text?: string; list?: Array<Task> }
 let id: number;
 id = 0;
 
+
 export class Liste extends React.Component<undefined,ListState> {
     //constructeur
     constructor() {
@@ -21,7 +22,10 @@ export class Liste extends React.Component<undefined,ListState> {
     //méthode appelée au clic sur le
     handleClick(event: any) : void {
         id++;
-        let tmp = new Task(this.state.text, id);
+        let tmp = new Task(this.state.text, id); // JON : Non. parce que Task est un composant. Tu dois cibler des data ici.
+        // JON : ton composant manipule des data et il utilise d'autre composant pour mettre en forme ces data
+        // JON : je conseille de déclarer une class Task
+        // JOn : class Task { id:string, name:string, isDone: boolean } puis ici tu fais ton new Task
         this.add(tmp);
     }
     //méthode appelée a chaque changement de texte dans la barre d'ajout de tache
@@ -38,7 +42,7 @@ export class Liste extends React.Component<undefined,ListState> {
             <ul>
                 {
                     this.state.list.map((t: Task) => {
-                        return <li key={t.props.id}>{t.render}</li>
+                        return <li key={t.props.id}>{t.render /* JON :  ici tu peux directment appeler ton composant <Task name={} id={} /> */ }</li> 
                     })
                 }
             </ul>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,21 +1,22 @@
 import * as React from "react";
 //import * as style from "../css/style.scss";
 
-interface TaskState {isDone ?: boolean}
+interface TaskState {isDone ?: boolean} {/* JON : jamais utilisé */}
 interface TaskProps {id ?: number; name ?: string;  }
 
-//class créant une tache 
+//class créant une tache -> JON : ça c'est un composant Taches = représentation visuelle d'une tache 
 export class Task extends React.Component<TaskProps, TaskState> {
     //constructeur
     constructor(taskname : string, idTask:number) {
         super();
-        this.state = {isDone : false};
+        this.state = {isDone : false}; // JON : ici tu gère le fait que la taches soit terminée par le composant lui même 
         this.props = {id : idTask, name: taskname}
     }
-    //classe permettant de lui attribuer une représentation graphique
+    // classe permettant de lui attribuer une représentation graphique -> JON :  c'est toute la classe qui gère la représentation graphique
+    // JON : cette classe gère uniquement le rendu
     render(): JSX.Element {
         return (<div>
-            <input type="radio"/>
+            <input type="radio"/> 
             <p>{this.props.name}</p>
         </div>)
     }
